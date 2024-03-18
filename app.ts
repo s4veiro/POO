3ba00adbd37a4362287fffd2700f12eef193fbdd
Gabriel Ribeiro <146039352+s4veiro@users.noreply.github.com>
2024-03-18 14:51:23 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,35 @@
+class Account {
+    name: string;
+    accountNumber: number;
+    balance: number = 0;
+
+    constructor(name: string, accountNumber: number){
+        this.name = name;
+        this.accountNumber = accountNumber;
+    }
+
+    deposit = () => {
+        console.log('Voce depositou.');
+    }
+
+    withdraw = () => {
+        console.log('Voce sacou.');
+    }
+
+    getBalance = () => {
+        console.log(this.balance);
+    }
+}
+
+class PeopleAccount extends Account {
+    doc_id: number;
+
+    constructor(doc_id: number, name: string, accountNumber: number){
+        super(name, accountNumber);
+        this.doc_id = doc_id;
+    }
+}
+
+const person1 = new PeopleAccount(3883, 'Gabriel', 3);
+
+console.log(person1);
